1d87f747bc560fedbd56c870140833bb3486120c
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-01-22 12:31:31 +0100

    Add some stuff for original reward signal runs

---
+++ vanilla_run.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+import logging
+import os
+import os.path as osp
+from multiprocessing import Queue
+
+import gym
+import gym_gridworld
+import params
+from baselines import bench, logger
+from baselines.a2c.a2c import learn
+from baselines.a2c.policies import CnnPolicy, LnLstmPolicy, LstmPolicy
+from baselines.common import set_global_seeds
+from baselines.common.atari_wrappers import wrap_deepmind
+from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
+
+
+def configure_logger(log_dir):
+    baselines_dir = osp.join(log_dir, 'baselines')
+    os.makedirs(baselines_dir)
+
+    json_file = open(osp.join(baselines_dir, 'progress.json'), 'wt')
+    json = logger.JSONOutputFormat(json_file)
+
+    tb = logger.TensorBoardOutputFormat(baselines_dir)
+
+    formats = [json, tb]
+    logger.Logger.CURRENT = logger.Logger(
+        dir=baselines_dir, output_formats=formats)
+
+def train(env_id, num_frames, seed, policy, lrschedule, num_cpu):
+    configure_logger('/tmp/run')
+    num_timesteps = int(num_frames / 4 * 1.1)
+
+    # divide by 4 due to frameskip, then do a little extras so episodes end
+    def make_env(rank):
+        def _thunk():
+            env = gym.make(env_id)
+            env.seed(seed + rank)
+            env = bench.Monitor(
+                env,
+                logger.get_dir() and os.path.join(
+                    logger.get_dir(), "{}.monitor.json".format(rank)))
+            gym.logger.setLevel(logging.WARN)
+            return wrap_deepmind(env)
+
+        return _thunk
+
+    set_global_seeds(seed)
+    env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
+    if policy == 'cnn':
+        policy_fn = CnnPolicy
+    elif policy == 'lstm':
+        policy_fn = LstmPolicy
+    elif policy == 'lnlstm':
+        policy_fn = LnLstmPolicy
+    seg_pipe = Queue()
+    go_pipe = Queue()
+    go_pipe.put(True)
+    learn(
+        policy_fn,
+        env,
+        seed,
+        seg_pipe,
+        go_pipe,
+        total_timesteps=num_timesteps,
+        lrschedule=lrschedule,
+        orig_rewards=True,
+        gen_segs=False,
+        log_dir='/tmp/run',
+        log_interval=10)
+    env.close()
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--env', help='environment ID', default='GridWorldNoFrameskip-v4')
+    parser.add_argument('--n_envs', type=int, default=4)
+    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
+    parser.add_argument(
+        '--policy',
+        help='Policy architecture',
+        choices=['cnn', 'lstm', 'lnlstm'],
+        default='cnn')
+    parser.add_argument(
+        '--lrschedule',
+        help='Learning rate schedule',
+        choices=['constant', 'linear'],
+        default='constant')
+    parser.add_argument(
+        '--million_frames',
+        help='How many frames to train (/ 1e6). '
+        'This number gets divided by 4 due to frameskip',
+        type=int,
+        default=40)
+    args = parser.parse_args()
+
+    params.init_params()
+    params.params['debug'] = True
+    params.params['print_lr'] = False
+
+    train(
+        args.env,
+        num_frames=1e6 * args.million_frames,
+        seed=args.seed,
+        policy=args.policy,
+        lrschedule=args.lrschedule,
+        num_cpu=args.n_envs)
+
+
+if __name__ == '__main__':
+    main()
