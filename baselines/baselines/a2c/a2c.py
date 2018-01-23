72d3e48986938ec13df6ed584a4d4ec0057a5f03
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-01-23 11:28:36 +0100

    Fix linter errors

--- baselines/baselines/a2c/a2c.py
+++ baselines/baselines/a2c/a2c.py
@@ -12,48 +12,61 @@ from baselines import logger
 from baselines.a2c.utils import (Scheduler, cat_entropy, discount_with_dones,
                                  find_trainable_variables, make_path, mse)
 from baselines.common import explained_variance, set_global_seeds
-from baselines.common.atari_wrappers import wrap_deepmind
-from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
-
-
 """
 - states: model state (e.g. LSTM state)
 - masks: is only used for stateful models
 """
 
-class Model(object):
 
-    def __init__(self, policy, ob_space, ac_space, nenvs, nsteps, nstack, num_procs,
-            ent_coef=0.01, vf_coef=0.5, max_grad_norm=0.5, lr=7e-4,
-            alpha=0.99, epsilon=1e-5, total_timesteps=int(80e6), lrschedule='linear'):
-        config = tf.ConfigProto(allow_soft_placement=True,
-                                intra_op_parallelism_threads=num_procs,
-                                inter_op_parallelism_threads=num_procs)
+class Model(object):
+    def __init__(self,
+                 policy,
+                 ob_space,
+                 ac_space,
+                 nenvs,
+                 nsteps,
+                 nstack,
+                 num_procs,
+                 ent_coef=0.01,
+                 vf_coef=0.5,
+                 max_grad_norm=0.5,
+                 lr=7e-4,
+                 alpha=0.99,
+                 epsilon=1e-5,
+                 total_timesteps=int(80e6),
+                 lrschedule='linear'):
+        config = tf.ConfigProto(
+            allow_soft_placement=True,
+            intra_op_parallelism_threads=num_procs,
+            inter_op_parallelism_threads=num_procs)
         config.gpu_options.allow_growth = True
         sess = tf.Session(config=config)
-        nact = ac_space.n
-        nbatch = nenvs*nsteps
+        nbatch = nenvs * nsteps
 
         A = tf.placeholder(tf.int32, [nbatch])
         ADV = tf.placeholder(tf.float32, [nbatch])
         R = tf.placeholder(tf.float32, [nbatch])
         LR = tf.placeholder(tf.float32, [])
 
-        step_model = policy(sess, ob_space, ac_space, nenvs, 1, nstack, reuse=False)
-        train_model = policy(sess, ob_space, ac_space, nenvs, nsteps, nstack, reuse=True)
+        step_model = policy(
+            sess, ob_space, ac_space, nenvs, 1, nstack, reuse=False)
+        train_model = policy(
+            sess, ob_space, ac_space, nenvs, nsteps, nstack, reuse=True)
 
-        neglogpac = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=train_model.pi, labels=A)
+        neglogpac = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            logits=train_model.pi, labels=A)
         pg_loss = tf.reduce_mean(ADV * neglogpac)
         vf_loss = tf.reduce_mean(mse(tf.squeeze(train_model.vf), R))
         entropy = tf.reduce_mean(cat_entropy(train_model.pi))
-        loss = pg_loss - entropy*ent_coef + vf_loss * vf_coef
+        loss = pg_loss - entropy * ent_coef + vf_loss * vf_coef
 
         params = find_trainable_variables("model")
         grads = tf.gradients(loss, params)
         if max_grad_norm is not None:
             grads, grad_norm = tf.clip_by_global_norm(grads, max_grad_norm)
         grads = list(zip(grads, params))
-        trainer = tf.train.RMSPropOptimizer(learning_rate=LR, decay=alpha, epsilon=epsilon)
+        trainer = tf.train.RMSPropOptimizer(
+            learning_rate=LR, decay=alpha, epsilon=epsilon)
         _train = trainer.apply_gradients(grads)
 
         lr = Scheduler(v=lr, nvalues=total_timesteps, schedule=lrschedule)
@@ -65,14 +78,18 @@ class Model(object):
             if run_params.params['print_lr']:
                 import datetime
                 print(str(datetime.datetime.now()), cur_lr)
-            td_map = {train_model.X:obs, A:actions, ADV:advs, R:rewards, LR:cur_lr}
+            td_map = {
+                train_model.X: obs,
+                A: actions,
+                ADV: advs,
+                R: rewards,
+                LR: cur_lr
+            }
             if states != []:
                 td_map[train_model.S] = states
                 td_map[train_model.M] = masks
             policy_loss, value_loss, policy_entropy, _ = sess.run(
-                [pg_loss, vf_loss, entropy, _train],
-                td_map
-            )
+                [pg_loss, vf_loss, entropy, _train], td_map)
             return policy_loss, value_loss, policy_entropy
 
         def save(save_path):
@@ -85,7 +102,7 @@ class Model(object):
             restores = []
             for p, loaded_p in zip(params, loaded_params):
                 restores.append(p.assign(loaded_p))
-            ps = sess.run(restores)
+            sess.run(restores)
 
         self.train = train
         self.train_model = train_model
@@ -97,16 +114,24 @@ class Model(object):
         self.load = load
         tf.global_variables_initializer().run(session=sess)
 
-class Runner(object):
 
-    def __init__(self, env, model, seg_pipe, log_dir, nsteps=5, nstack=4, gamma=0.99,
-            orig_rewards=False, gen_segs=True):
+class Runner(object):
+    def __init__(self,
+                 env,
+                 model,
+                 seg_pipe,
+                 log_dir,
+                 nsteps=5,
+                 nstack=4,
+                 gamma=0.99,
+                 orig_rewards=False,
+                 gen_segs=True):
         self.env = env
         self.model = model
         nh, nw, nc = env.observation_space.shape
         nenv = env.num_envs
-        self.batch_ob_shape = (nenv*nsteps, nh, nw, nc*nstack)
-        self.obs = np.zeros((nenv, nh, nw, nc*nstack), dtype=np.uint8)
+        self.batch_ob_shape = (nenv * nsteps, nh, nw, nc * nstack)
+        self.obs = np.zeros((nenv, nh, nw, nc * nstack), dtype=np.uint8)
         # The first stack of 4 frames: the first 3 frames are zeros,
         # with the last frame coming from env.reset().
         obs = env.reset()
@@ -135,7 +160,6 @@ class Runner(object):
             osp.join(log_dir, 'true_reward'), flush_secs=5)
         self.sess.run([op.initializer for op in self.tr_ops])
 
-
     def update_obs(self, obs):
         # Do frame-stacking here instead of the FrameStack wrapper to reduce
         # IPC overhead
@@ -148,27 +172,29 @@ class Runner(object):
         e0_obs = mb_obs[0]
         e0_dones = mb_dones[0]
         assert_equal(e0_obs.shape, (self.nsteps, 84, 84, 4))
-        assert_equal(e0_dones.shape, (self.nsteps,))
+        assert_equal(e0_dones.shape, (self.nsteps, ))
 
         for step in range(self.nsteps):
             if len(self.segment) == 25:
-                #print("New segment generated")
                 self.seg_pipe.put(self.segment)
                 self.segment = []
                 continue
             elif e0_dones[step]:
                 # The last segment will probably not be 25 steps long;
-                # drop it, so that all segments in the batch are the same length
+                # drop it, so that all segments in the batch are the same
+                # length
                 self.segment = []
                 continue
             self.segment.append(e0_obs[step])
 
     def run(self):
         nenvs = len(self.env.remotes)
-        mb_obs, mb_rewards, mb_actions, mb_values, mb_dones = [],[],[],[],[]
+        mb_obs, mb_rewards, mb_actions, mb_values, mb_dones = \
+            [], [], [], [], []
         mb_states = self.states
         for n in range(self.nsteps):
-            actions, values, states = self.model.step(self.obs, self.states, self.dones)
+            actions, values, states = self.model.step(self.obs, self.states,
+                                                      self.dones)
             mb_obs.append(np.copy(self.obs))
             mb_actions.append(actions)
             mb_values.append(values)
@@ -179,14 +205,14 @@ class Runner(object):
             self.dones = dones
             for n, done in enumerate(dones):
                 if done:
-                    self.obs[n] = self.obs[n]*0
+                    self.obs[n] = self.obs[n] * 0
                     if run_params.params['debug']:
                         print("Env %d done" % n)
             # SubprocVecEnv automatically resets when done
             self.update_obs(obs)
             mb_rewards.append(rewards)
         mb_dones.append(self.dones)
-        #batch of steps to batch of rollouts
+        # batch of steps to batch of rollouts
         # i.e. from nsteps, nenvs to nenvs, nsteps
         mb_obs = np.asarray(mb_obs, dtype=np.uint8).swapaxes(1, 0)
         mb_rewards = np.asarray(mb_rewards, dtype=np.float32).swapaxes(1, 0)
@@ -207,7 +233,9 @@ class Runner(object):
                 if run_params.params['debug']:
                     print("Env %d" % env_n)
                     print(mb_actions[env_n])
-                    print([self.env.action_meanings[i] for i in mb_actions[env_n]])
+                    print([
+                        self.env.action_meanings[i] for i in mb_actions[env_n]
+                    ])
                     print(mb_rewards[env_n])
         else:
             # for the data from each environment
@@ -217,9 +245,11 @@ class Runner(object):
                 if run_params.params['debug']:
                     print("Env %d" % env_n)
                     print(mb_actions[env_n])
-                    print([self.env.action_meanings[i] for i in mb_actions[env_n]])
+                    print([
+                        self.env.action_meanings[i] for i in mb_actions[env_n]
+                    ])
                 rewards = self.reward_model.reward(obs)
-                assert_equal(rewards.shape, (self.nsteps,))
+                assert_equal(rewards.shape, (self.nsteps, ))
                 mb_rewards[env_n] = rewards
 
         for env_n, (rs, dones) in enumerate(zip(mb_rewards, mb_dones)):
@@ -231,8 +261,8 @@ class Runner(object):
                     if run_params.params['debug']:
                         print("Env %d: episode finished, true reward %d" %
                               (env_n, self.true_reward[env_n]))
-                    self.sess.run(
-                        self.tr_ops[env_n].assign(self.true_reward[env_n]))
+                    self.sess.run(self.tr_ops[env_n].assign(
+                        self.true_reward[env_n]))
                     summ = self.sess.run(self.summ_ops[env_n])
                     self.writer.add_summary(summ, self.n_episodes[env_n])
 
@@ -241,16 +271,19 @@ class Runner(object):
 
         mb_obs = mb_obs.reshape(self.batch_ob_shape)
 
-        last_values = self.model.value(self.obs, self.states, self.dones).tolist()
-        #discount/bootstrap off value fn
-        for n, (rewards, dones, value) in enumerate(zip(mb_rewards, mb_dones, last_values)):
+        last_values = self.model.value(self.obs, self.states,
+                                       self.dones).tolist()
+        # discount/bootstrap off value fn
+        for n, (rewards, dones, value) in enumerate(
+                zip(mb_rewards, mb_dones, last_values)):
             rewards = rewards.tolist()
             dones = dones.tolist()
             if dones[-1] == 0:
                 # Make sure that the first iteration of the loop inside
                 # discount_with_dones picks up 'value' as the initial
                 # value of r
-                rewards = discount_with_dones(rewards+[value], dones+[0], self.gamma)[:-1]
+                rewards = discount_with_dones(rewards + [value], dones + [0],
+                                              self.gamma)[:-1]
             else:
                 rewards = discount_with_dones(rewards, dones, self.gamma)
             mb_rewards[n] = rewards
@@ -260,11 +293,29 @@ class Runner(object):
         mb_masks = mb_masks.flatten()
         return mb_obs, mb_states, mb_rewards, mb_masks, mb_actions, mb_values
 
-def learn(policy, env, seed, seg_pipe, go_pipe, log_dir, nsteps=5, nstack=4,
-        total_timesteps=int(80e6), vf_coef=0.5, ent_coef=0.01,
-        max_grad_norm=0.5, lr=7e-4, lrschedule='linear', epsilon=1e-5,
-        alpha=0.99, gamma=0.99, log_interval=100, load=False,
-        save_interval=10000, orig_rewards=False, gen_segs=True):
+
+def learn(policy,
+          env,
+          seed,
+          seg_pipe,
+          go_pipe,
+          log_dir,
+          nsteps=5,
+          nstack=4,
+          total_timesteps=int(80e6),
+          vf_coef=0.5,
+          ent_coef=0.01,
+          max_grad_norm=0.5,
+          lr=7e-4,
+          lrschedule='linear',
+          epsilon=1e-5,
+          alpha=0.99,
+          gamma=0.99,
+          log_interval=100,
+          load=False,
+          save_interval=10000,
+          orig_rewards=False,
+          gen_segs=True):
 
     tf.reset_default_graph()
     set_global_seeds(seed)
@@ -272,9 +323,25 @@ def learn(policy, env, seed, seg_pipe, go_pipe, log_dir, nsteps=5, nstack=4,
     nenvs = env.num_envs
     ob_space = env.observation_space
     ac_space = env.action_space
-    num_procs = len(env.remotes) # HACK
+    num_procs = len(env.remotes)  # HACK
 
-    make_model = lambda: Model(policy=policy, ob_space=ob_space, ac_space=ac_space, nenvs=nenvs, nsteps=nsteps, nstack=nstack, num_procs=num_procs, ent_coef=ent_coef, vf_coef=vf_coef, max_grad_norm=max_grad_norm, lr=lr, alpha=alpha, epsilon=epsilon, total_timesteps=total_timesteps, lrschedule=lrschedule)
+    def make_model():
+        return Model(
+            policy=policy,
+            ob_space=ob_space,
+            ac_space=ac_space,
+            nenvs=nenvs,
+            nsteps=nsteps,
+            nstack=nstack,
+            num_procs=num_procs,
+            ent_coef=ent_coef,
+            vf_coef=vf_coef,
+            max_grad_norm=max_grad_norm,
+            lr=lr,
+            alpha=alpha,
+            epsilon=epsilon,
+            total_timesteps=total_timesteps,
+            lrschedule=lrschedule)
 
     if save_interval and logger.get_dir():
         import cloudpickle
@@ -283,18 +350,26 @@ def learn(policy, env, seed, seg_pipe, go_pipe, log_dir, nsteps=5, nstack=4,
 
     print("Initialising model...")
     model = make_model()
-    runner = Runner(env, model, seg_pipe, log_dir, nsteps=nsteps, nstack=nstack,
-            gamma=gamma, orig_rewards=orig_rewards, gen_segs=gen_segs)
+    runner = Runner(
+        env,
+        model,
+        seg_pipe,
+        log_dir,
+        nsteps=nsteps,
+        nstack=nstack,
+        gamma=gamma,
+        orig_rewards=orig_rewards,
+        gen_segs=gen_segs)
 
     print("Running...")
 
     # nsteps: e.g. 5
     # nenvs: e.g. 16
-    nbatch = nenvs*nsteps
+    nbatch = nenvs * nsteps
     fps_tstart = time.time()
     fps_nsteps = 0
     train = False
-    for update in range(1, total_timesteps//nbatch+1):
+    for update in range(1, total_timesteps // nbatch + 1):
         # Run for nsteps
         obs, states, rewards, masks, actions, values = runner.run()
 
@@ -321,7 +396,7 @@ def learn(policy, env, seed, seg_pipe, go_pipe, log_dir, nsteps=5, nstack=4,
 
             ev = explained_variance(values, rewards)
             logger.record_tabular("nupdates", update)
-            logger.record_tabular("total_timesteps", update*nbatch)
+            logger.record_tabular("total_timesteps", update * nbatch)
             logger.record_tabular("fps", fps)
             logger.record_tabular("policy_entropy", float(policy_entropy))
             logger.record_tabular("value_loss", float(value_loss))
