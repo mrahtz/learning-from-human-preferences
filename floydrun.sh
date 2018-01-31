d93aac32cd57a9dd008ade63d7656fee1f9577b7
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-01-31 19:58:49 +0100

    Set up for floyd

---
+++ floydrun.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/openai/gym \
+    && cd gym \
+    && git reset --hard b5576dc23a5fcad0733042ab2ad440200ebb6209 \
+    && pip install -e .[atari] \
+    && cd ../gym-gridworld \
+    && pip install -e . \
+    && cd ../baselines \
+    && pip install --upgrade cython cloudpickle && pip install -e . \
+    && cd .. \
+    && python run.py $*
