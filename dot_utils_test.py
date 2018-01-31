371ef13088f2dc79769504f199dc0480f8be5fa6
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-01-31 12:28:54 +0100

    Tidy up

--- dot_utils_test.py
+++ dot_utils_test.py
@@ -8,8 +8,7 @@ import numpy as np
 import gym
 import gym_gridworld  # noqa: F401 (imported but unused)
 from baselines.common.atari_wrappers import wrap_deepmind_nomax
-from dot_utils import predict_action_preference, predict_action_rewards
-from dot_utils import get_coords
+from dot_utils import predict_action_preference
 
 
 class TestDotUtils(unittest.TestCase):
@@ -18,61 +17,46 @@ class TestDotUtils(unittest.TestCase):
         pass
 
     def test_predict_action_preferences(self):
+        """
+        Simulate a few episodes of the agent wandering around randomly and
+        check whether the reward predictor's preferences about episodes is
+        correct.
+        """
         env = wrap_deepmind_nomax(gym.make("GridWorldNoFrameskip-v4"))
-
-        segments = []
-        r_sums = []
-        for run_n in range(5):
+        episodes = []
+        episode_rewards = []
+        for _ in range(5):
             segment = []
             obs = env.reset()
             segment.append(obs)
             done = False
-            actual_rs = []
+            rewards = []
             while not done:
-                a_probs = np.arange(start=1, stop=6)
+                # The reward predictor might be wrong when the dot is close to
+                # the middle because of errors introduced by the frame scaling.
+                # Therefore we shouldn't use a uniform distribution, because it
+                # could easily get net zero reward (from being equally likely
+                # to take steps in good directions as bad direction). Instead
+                # we use a linear distribution.
+                a_probs = np.arange(start=1, stop=(env.action_space.n + 1))
                 a_probs = a_probs / sum(a_probs)
                 action = np.random.choice(np.arange(5), p=a_probs)
                 o, r, done, _ = env.step(action)
                 segment.append(o)
+                rewards.append(r)
+            episode_rewards.append(sum(rewards))
+            episodes.append(np.copy(segment))
 
-                action_name = env.unwrapped.get_action_meanings()[action]
-                print("I took action", action_name, "and now I'm at",
-                      env.unwrapped.pos)
-                print(np.sum(o))
-                print("Predict thinks I'm at", get_coords(o))
-
-                predicted_r = predict_action_rewards(segment[-2:])
-                print(r, predicted_r)
-                """
-                if r != int(predicted_r[0]):
-                    print("ERR")
-                    from IPython.core.debugger import Pdb
-                    Pdb().set_trace()
-                """
-
-                actual_rs.append(r)
-            predicted_rs = predict_action_rewards(segment)
-            print(actual_rs[:10])
-            print(predict_action_rewards(segment)[:10])
-            n_errs = sum([abs(a - b) for a, b in zip(predicted_rs, actual_rs)])
-            print(n_errs)
-            r_sums.append(sum(actual_rs))
-            segments.append(np.copy(segment))
-
-        for i1, i2 in list(combinations(range(len(segments)), 2)):
-            print("Segments %d and %d" % (i1, i2))
-            print("Expected:", r_sums[i1], r_sums[i2])
-            s1, s2 = segments[i1], segments[i2]
-            print("Actual:", sum(predict_action_rewards(s1)),
-                  sum(predict_action_rewards(s2)))
-            actual_mu = predict_action_preference(s1, s2)
-            if r_sums[i1] > r_sums[i2]:
-                expected_mu = (1.0, 0.0)
-            elif r_sums[i1] < r_sums[i2]:
-                expected_mu = (0.0, 1.0)
+        for i1, i2 in list(combinations(range(len(episodes)), 2)):
+            e1, e2 = episodes[i1], episodes[i2]
+            actual_pref = predict_action_preference(e1, e2)
+            if episode_rewards[i1] > episode_rewards[i2]:
+                expected_pref = (1.0, 0.0)
+            elif episode_rewards[i1] < episode_rewards[i2]:
+                expected_pref = (0.0, 1.0)
             else:
-                expected_mu = (0.5, 0.5)
-            self.assertEqual(actual_mu, expected_mu)
+                expected_pref = (0.5, 0.5)
+            self.assertEqual(actual_pref, expected_pref)
 
 
 if __name__ == '__main__':
