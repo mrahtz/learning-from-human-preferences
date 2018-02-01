da80ef6f69d454c15b98874e1ffa531a9d92e3e9
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-02-01 17:37:53 +0100

    Clean up

--- floyd.sh
+++ floyd.sh
@@ -1,10 +1,20 @@
-git clone https://github.com/openai/gym \
-    && cd gym \
-    && git reset --hard b5576dc23a5fcad0733042ab2ad440200ebb6209 \
-    && pip install -e .[atari] \
-    && cd ../gym-gridworld \
-    && pip install -e . \
-    && cd ../baselines \
-    && pip install --upgrade cython cloudpickle && pip install -e . \
-    && cd .. \
-    && $*
+#!/bin/bash
+
+set -o errexit
+
+git clone https://github.com/openai/gym
+cd gym
+git reset --hard b5576dc23a5fcad0733042ab2ad440200ebb6209
+pip install -e .[atari]
+cd ..
+
+cd gym-gridworld
+pip install -e .
+cd ..
+
+cd baselines
+pip install --upgrade cython cloudpickle
+pip install -e .
+cd ..
+
+$*
