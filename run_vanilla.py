c1720386777be8dd93e2efbee221ca0a3f1f9a92
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-02-07 11:06:10 +0100

    Lower learning rate

--- run_vanilla.py
+++ run_vanilla.py
@@ -72,7 +72,8 @@ def train(env_id, num_frames, seed, policy, lrschedule, num_cpu, log_dir):
         orig_rewards=True,
         gen_segs=False,
         log_dir=log_dir,
-        log_interval=10)
+        log_interval=10,
+        lr=3e-4)
     env.close()
 
 
