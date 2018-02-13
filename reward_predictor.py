2a6d56a94bbee9725d4ad191959ea3438e00ef12
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-02-13 16:27:29 +0100

    Bigger network

--- reward_predictor.py
+++ reward_predictor.py
@@ -141,7 +141,9 @@ def net_easyfeatures(s, reuse):
     x = tf.stack(features, axis=1)
 
     x = dense_layer(x, 64, "d1", reuse, activation='relu')
-    x = dense_layer(x, 1, "d2", reuse, activation=None)
+    x = dense_layer(x, 64, "d2", reuse, activation='relu')
+    x = dense_layer(x, 64, "d3", reuse, activation='relu')
+    x = dense_layer(x, 1, "d4", reuse, activation=None)
     x = x[:, 0]
 
     return x
