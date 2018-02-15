8890a2dfce485942bb5a33081f7909ba230c31a8
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-02-15 09:44:15 +0100

    Simplify

--- floyd.sh
+++ floyd.sh
@@ -2,19 +2,21 @@
 
 set -o errexit
 
+find . | xargs touch
+
 git clone https://github.com/openai/gym
 cd gym
 git reset --hard b5576dc23a5fcad0733042ab2ad440200ebb6209
-pip install -e .[atari]
+pip install .[atari]
 cd ..
 
 cd gym-gridworld
-pip install -e .
+pip install .
 cd ..
 
 cd baselines
 pip install --upgrade cython cloudpickle
-pip install -e .
+pip install .
 cd ..
 
 $*
