4db0c2f82f02ee660730e05ce7f46055a2f3c1f4
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-02-22 12:41:01 +0100

    Remove None filtering code

--- reward_predictor.py
+++ reward_predictor.py
@@ -240,13 +240,6 @@ def train_reward_predictor(reward_predictor, lr, pref_pipe, go_pipe,
         pref_db_val = PrefDB()
         pref_db_train = PrefDB()
 
-    for pref_n, pref in enumerate(pref_db_train.prefs):
-        if pref[2] is None:
-            pref_db_train.del_pref(pref_n)
-    for pref_n, pref in enumerate(pref_db_val.prefs):
-        if pref[2] is None:
-            pref_db_val.del_pref(pref_n)
-
     # Page 15: "We collect 500 comparisons from a randomly initialized policy
     # network at the beginning of training"
     if not params.params['skip_prefs']:
