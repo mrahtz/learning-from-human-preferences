4fed7abcd4e5f8fb0f21dca76af56afe4db452a2
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-02-27 10:55:14 +0100

    Fix gridworld install

--- gym-gridworld/setup.py
+++ gym-gridworld/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='gym_gridworld',
       version='1.0',
-      packages=['gym_gridworld'],
+      packages=['gym_gridworld', 'gym_gridworld.envs'],
       install_requires=['gym', 'numpy']
 )
