1168570807b755a5aa058a284d9680bbd1df4abf
Matthew Rahtz <matthew.rahtz@gmail.com>
2018-03-02 12:20:18 +0100

    Fix typo

--- create_floyd_base.sh
+++ create_floyd_base.sh
@@ -6,6 +6,6 @@ touch before_file
 
 bash floyd_wrapper.sh
 
-echo "Copying changes files..."
+echo "Copying changed files..."
 find / -type f -newer before_file | grep -v -e '^/proc' -e '^/sys' -e '^/output' -e '^/code' -e '^/floydlocaldata' -e '^/root' | xargs -i cp --parents {} /output
 echo "Done!"
